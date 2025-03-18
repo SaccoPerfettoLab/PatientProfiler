92bc729b1cfc93d7581559adbba2e228c450877c
VeronicaLombardi <veronica.lombardi98@gmail.com>
2025-03-18 10:50:00 +0100

    exported functions

--- R/query_uniprot.R
+++ R/query_uniprot.R
@@ -1,63 +1,76 @@
 
-query_uniprot_proteins <- function(id_input = uniprot_ids, batch_size = 400){
-
-  # Keep only ID input that are UNIPROT IDs
+#' Query UNIPROT Proteins
+#'
+#' This function retrieves protein-related information from the UNIPROT database.
+#' It performs batch queries to efficiently retrieve data.
+#'
+#' @param id_input A vector of UNIPROT IDs.
+#' @param batch_size An integer specifying the number of IDs per query batch (default is 400).
+#' @return A dataframe with UNIPROT information including accession, gene names, protein names, and sequence details.
+#' @export
+query_uniprot_proteins <- function(id_input, batch_size = 400){
+  
+  # Filter only valid UNIPROT IDs
   id_input <- unique(id_input)
   id_input <- id_input[grepl('[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}', id_input)]
   id_input <- id_input[!grepl('CHEBI:|SIGNOR-|NP_|CID:|SID:|PRO_|DB0|_9606', id_input)]
-
-  header_df_uni2seq_fin <- c("Entry", "Reviewed", "Entry Name", "Protein names", "Gene Names (primary)", "Organism", "Length", "Sequence", "Date_modified")
-  df_uni2seq_fin <- data.frame(matrix(ncol = 9, nrow = 0))
-  colnames(df_uni2seq_fin) <- header_df_uni2seq_fin
-
-  for (i in seq(from= 1, to= length(id_input)-(batch_size-1), by = batch_size)){
-    print (i)
-    id= unique(id_input[i:(i+(batch_size-1))])
-
-    query_test= paste0('accession%3A', paste0( id, collapse ='%20OR%20accession%3A'))
-    url_test=paste0('https://rest.uniprot.org/uniprotkb/stream?fields=accession%2Creviewed%2Cid%2Cprotein_name%2Cgene_primary%2Corganism_name%2Clength%2Csequence%2Cdate_sequence_modified&format=tsv&query=',query_test)
-    result <- httr::GET(url_test)
-    as.character(httr::content(result, "text"))-> file_uni# automatically parses JSON
-    df_uni2seq <- readr::read_delim(file_uni, delim = '\t',skip_empty_rows = TRUE, show_col_types =  F)
-    df_uni2seq_fin <- rbind(df_uni2seq_fin, df_uni2seq )
+  
+  # Define column names for the final dataframe
+  header_df <- c("Entry", "Reviewed", "Entry Name", "Protein names", "Gene Names (primary)", "Organism", "Length", "Sequence", "Date_modified")
+  df_final <- data.frame(matrix(ncol = length(header_df), nrow = 0))
+  colnames(df_final) <- header_df
+  
+  for (i in seq(1, length(id_input), by = batch_size)) {
+    print(paste("Processing batch starting at index:", i))
+    batch_ids <- unique(id_input[i:min(i + batch_size - 1, length(id_input))])
+    
+    query_string <- paste0('accession%3A', paste0(batch_ids, collapse = '%20OR%20accession%3A'))
+    url <- paste0('https://rest.uniprot.org/uniprotkb/stream?fields=accession%2Creviewed%2Cid%2Cprotein_name%2Cgene_primary%2Corganism_name%2Clength%2Csequence%2Cdate_sequence_modified&format=tsv&query=', query_string)
+    
+    result <- httr::GET(url)
+    file_content <- as.character(httr::content(result, "text"))
+    
+    df_batch <- readr::read_delim(file_content, delim = '\t', skip_empty_rows = TRUE, show_col_types = FALSE)
+    df_final <- dplyr::bind_rows(df_final, df_batch)
   }
-
-  id= unique(id_input[i:length(id_input)])
-  query_test= paste0('accession%3A', paste0( id, collapse ='%20OR%20accession%3A'))
-  url_test=paste0('https://rest.uniprot.org/uniprotkb/stream?fields=accession%2Creviewed%2Cid%2Cprotein_name%2Cgene_primary%2Corganism_name%2Clength%2Csequence%2Cdate_sequence_modified&format=tsv&query=',query_test)
-  result <- httr::GET(url_test)
-  as.character(httr::content(result, "text"))-> file_uni# automatically parses JSON
-  df_uni2seq <- readr::read_delim(file_uni, delim = '\t',skip_empty_rows = TRUE,show_col_types = F)
-  df_uni2seq_fin <- rbind(df_uni2seq_fin,df_uni2seq ) %>% dplyr::distinct()
-
-  return(df_uni2seq_fin)
+  
+  return(df_final %>% dplyr::distinct())
 }
 
+#' Query UNIPROT
+#'
+#' Retrieves protein data from UNIPROT, handling isoforms separately.
+#'
+#' @param id_input A vector of UNIPROT IDs.
+#' @param batch_size Number of IDs per batch (default = 400).
+#' @return A dataframe with retrieved protein data.
+#' @export
 query_uniprot <- function(id_input, batch_size = 400) {
-
+  
   id_input <- unique(id_input)
   id_input <- id_input[grepl('[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}', id_input)]
   id_input <- id_input[!grepl('CHEBI:|SIGNOR-|NP_|CID:|SID:|PRO_|DB0|_9606|VAR|REV', id_input)]
-
-  # Get isoforms
+  
+  # Separate isoforms
   id_input_iso <- unique(id_input[grepl('-', id_input)])
-
-  if(length(id_input_iso) != 0){ # If there are isoforms do a separate query for them
-    if(length(id_input_iso) > 400) {
+  
+  if (length(id_input_iso) != 0) {
+    if (length(id_input_iso) > batch_size) {
       result_iso <- query_uniprot_proteins(id_input = id_input_iso, batch_size = batch_size)
-    }else{
-      result_iso <- query_uniprot_proteins(id_input = id_input_iso, batch_size = length(id_input_iso)-1)
+    } else {
+      result_iso <- query_uniprot_proteins(id_input = id_input_iso, batch_size = length(id_input_iso))
     }
   }
-
-  result_proteins <- query_uniprot_proteins(id_input, batch_size) # Query for not isoforms
-
-  if(exists('result_iso')){
+  
+  # Query for non-isoform proteins
+  result_proteins <- query_uniprot_proteins(id_input, batch_size)
+  
+  # Merge results
+  if (exists('result_iso')) {
     result_total <- dplyr::bind_rows(result_iso, result_proteins)
-  }else{
+  } else {
     result_total <- result_proteins
   }
-
+  
   return(result_total)
 }
-
