483cfb4ed3c34942b2829f9275b69b61a4b3e580
EleonoraMeo <eleonorameo.hp@gmail.com>
2025-03-18 14:12:10 +0100

    added dplyr on select, distinct, group_by, relocate, left_join, summarize

--- R/remove_duplicates_phosphoproteomics.R
+++ R/remove_duplicates_phosphoproteomics.R
@@ -34,24 +34,24 @@ remove_duplicates_phosphoproteomics <- function(df) {
   }
 
   df_uni <- df_unique %>%
-    group_by(key) %>%
-    dplyr::summarise(UNIPROT = paste(unique(UNIPROT), collapse = ";"), .groups = 'drop')
+    dplyr::group_by(key) %>%
+    dplyr::summarize(UNIPROT = paste(unique(UNIPROT), collapse = ";"), .groups = 'drop')
 
-  df_unique <- df_unique %>% select(-UNIPROT)
+  df_unique <- df_unique %>% dplyr::select(-UNIPROT)
 
   df_final <- df_unique %>%
-    left_join(df_uni, by = "key") %>% relocate(UNIPROT)
+    dplyr::left_join(df_uni, by = "key") %>% dplyr::relocate(UNIPROT)
 
   aggregate_uniprot <- function(df) {
     df %>%
-      group_by(gene_name) %>%
-      dplyr::summarise(UNIPROT_combined = paste(unique(unlist(strsplit(UNIPROT, ";"))), collapse = ";"), .groups = 'drop')
+      dplyr::group_by(gene_name) %>%
+      dplyr::summarize(UNIPROT_combined = paste(unique(unlist(strsplit(UNIPROT, ";"))), collapse = ";"), .groups = 'drop')
   }
 
   df_final <- df_final %>%
-    left_join(aggregate_uniprot(df_final), by = "gene_name") %>%
-    mutate(UNIPROT = UNIPROT_combined) %>%
-    select(-UNIPROT_combined)
+    dplyr::left_join(aggregate_uniprot(df_final), by = "gene_name") %>%
+    dplyr::mutate(UNIPROT = UNIPROT_combined) %>%
+    dplyr::select(-UNIPROT_combined)
 
   df_final <- unique(df_final)
 
@@ -63,30 +63,30 @@ remove_duplicates_phosphoproteomics <- function(df) {
   }
 
   df_uni <- df_final %>%
-    group_by(key) %>%
-    dplyr::summarise(gene_name = paste(unique(gene_name), collapse = ";"), .groups = 'drop')
+    dplyr::group_by(key) %>%
+    dplyr::summarize(gene_name = paste(unique(gene_name), collapse = ";"), .groups = 'drop')
 
-  df_final <- df_final %>% select(-gene_name)
+  df_final <- df_final %>% dplyr::select(-gene_name)
 
   df_final <- df_final %>%
-    left_join(df_uni, by = "key") %>% relocate(gene_name)
+    dplyr::left_join(df_uni, by = "key") %>% dplyr::relocate(gene_name)
 
   aggregate_gene <- function(df) {
     df %>%
-      group_by(UNIPROT) %>%
-      dplyr::summarise(gene_combined = paste(unique(unlist(strsplit(gene_name, ";"))), collapse = ";"), .groups = 'drop')
+      dplyr::group_by(UNIPROT) %>%
+      dplyr::summarize(gene_combined = paste(unique(unlist(strsplit(gene_name, ";"))), collapse = ";"), .groups = 'drop')
   }
 
   df_final <- df_final %>%
-    left_join(aggregate_gene(df_final), by = "UNIPROT") %>%
-    mutate(gene_name = gene_combined) %>%
-    select(-gene_combined)
+    dplyr::left_join(aggregate_gene(df_final), by = "UNIPROT") %>%
+    dplyr::mutate(gene_name = gene_combined) %>%
+    dplyr::select(-gene_combined)
 
   df_final <- unique(df_final)
 
   if ("sequence_window" %in% colnames(df)) {
     df_final_clean <- df_final %>%
-      filter(!is.na(sequence_window))
+      dplyr::filter(!is.na(sequence_window))
   } else {
     df_final_clean <- df_final
   }
@@ -98,7 +98,7 @@ remove_duplicates_phosphoproteomics <- function(df) {
     df_final_clean$key <- paste0(df_final_clean$gene_name, "_", df_final_clean$aminoacid, "_", df_final_clean$position)
   }
 
-  df_final_clean <- df_final_clean %>% relocate(key)
+  df_final_clean <- df_final_clean %>% dplyr::relocate(key)
 
 
   if ("sequence_window" %in% colnames(df_final_clean)) {
@@ -113,40 +113,40 @@ remove_duplicates_phosphoproteomics <- function(df) {
 
 
   df_final_clean2 <- df_final_clean %>%
-    group_by(key) %>%
-    dplyr::summarize(across(selected_cols, mean, na.rm = TRUE), .groups = 'drop')
+    dplyr::group_by(key) %>%
+    dplyr::summarize(dplyr::across(selected_cols, mean, na.rm = TRUE), .groups = 'drop')
 
 
   if ("sequence_window" %in% colnames(df_final_clean)) {
     metadata_columns <- df_final_clean %>%
-      select(key, gene_name, UNIPROT, aminoacid, position, sequence_window) %>%
-      distinct(key, .keep_all = TRUE)
+      dplyr::select(key, gene_name, UNIPROT, aminoacid, position, sequence_window) %>%
+      dplyr::distinct(key, .keep_all = TRUE)
 
   }else {
     metadata_columns <- df_final_clean %>%
-      select(key, gene_name, UNIPROT, aminoacid, position) %>%
-      distinct(key, .keep_all = TRUE)
+      dplyr::select(key, gene_name, UNIPROT, aminoacid, position) %>%
+      dplyr::distinct(key, .keep_all = TRUE)
 
   }
 
   if ("sequence_window" %in% colnames(df_final_clean)) {
 
     df_final_clean2 <- df_final_clean2 %>%
-      left_join(metadata_columns, by = "key") %>%
-      select(-key) %>%
-      relocate(UNIPROT, aminoacid, position, gene_name, sequence_window)
+      dplyr::left_join(metadata_columns, by = "key") %>%
+      dplyr::select(-key) %>%
+      dplyr::relocate(UNIPROT, aminoacid, position, gene_name, sequence_window)
 
   } else {
 
     df_final_clean2 <- df_final_clean2 %>%
-      left_join(metadata_columns, by = "key") %>%
-      select(-key) %>%
-      relocate(UNIPROT, aminoacid, position, gene_name)
+      dplyr::left_join(metadata_columns, by = "key") %>%
+      dplyr::select(-key) %>%
+      dplyr::relocate(UNIPROT, aminoacid, position, gene_name)
   }
 
 
   if ("sequence_window" %in% colnames(df)) {
-    df_final_clean2 <- df_final_clean2 %>% relocate(sequence_window, .after = UNIPROT)
+    df_final_clean2 <- df_final_clean2 %>% dplyr::relocate(sequence_window, .after = UNIPROT)
   }
 
   df_final_clean2 <- df_final_clean2 %>%
