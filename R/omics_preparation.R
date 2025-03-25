780caab6de32dc525838a2eae6fcded8cd24a3a6
veronicavenafra <veronica.venafra97@gmail.com>
2025-03-25 22:54:05 +0100

    absence of sequence_window handled without creating int

--- R/omics_preparation.R
+++ R/omics_preparation.R
@@ -32,15 +32,15 @@ omics_preparation <- function(
     prot_dir_name = "Prot_patients",
     phospho_dir_name = "Phospho_patients"
 ) {
-  
+
   if (!is.null(df_tr_updated)) {
     message("Transcriptomic data patients preparation...")
-    
+
     if (dir.exists(transc_dir_name)) {
-      unlink(transc_dir_name, recursive = TRUE)  
+      unlink(transc_dir_name, recursive = TRUE)
     }
-    dir.create(transc_dir_name)  
-    
+    dir.create(transc_dir_name)
+
     for (c in 2:ncol(df_tr_updated)) {
       df_tr_updated_1 = df_tr_updated[, c(1, c)]
       df_tr_updated_2 = cbind(gene_ID = df_tr_updated[, 1], df_tr_updated_1)
@@ -55,18 +55,18 @@ omics_preparation <- function(
     }
     message("Done!")
   }
-  
+
   if (!is.null(df_pr_updated)) {
     message("Proteomic data patients preparation...")
-    
+
     if (dir.exists(prot_dir_name)) {
       unlink(prot_dir_name, recursive = TRUE)
     }
-    dir.create(prot_dir_name)  
-    
+    dir.create(prot_dir_name)
+
     df_pr_updated <- df_pr_updated %>%
       dplyr::select(UNIPROT, everything())
-    
+
     for (i in 3:length(df_pr_updated)) {
       pt1 = df_pr_updated[, c(1:2)]
       patients = cbind(pt1, df_pr_updated[, i])
@@ -78,28 +78,31 @@ omics_preparation <- function(
     }
     message("Done!")
   }
-  
+
   if (!is.null(df_ph_updated)) {
     message("Phosphoproteomics data patients preparation...")
-    
+
     if (dir.exists(phospho_dir_name)) {
-      unlink(phospho_dir_name, recursive = TRUE)  
+      unlink(phospho_dir_name, recursive = TRUE)
     }
-    dir.create(phospho_dir_name)  
-    
-    if (!"sequence_window" %in% names(df_ph_updated)) {
-      df_ph_updated$sequence_window <- NA 
+    dir.create(phospho_dir_name)
+
+    if ("sequence_window" %in% names(df_ph_updated)) {
+      column_order <- c("UNIPROT", "aminoacid", "position", "gene_name", "sequence_window")
+    }else{
+      column_order <- c("UNIPROT", "aminoacid", "position", "gene_name")
     }
-    
-    column_order <- c("UNIPROT", "aminoacid", "position", "gene_name", "sequence_window")
+
     other_columns <- setdiff(names(df_ph_updated), column_order)
+    # Patients' values start after n° patients - n° columns reporting phosphosites information
+    start_pos <- ncol(df_ph_updated) - length(other_columns) + 1
     new_order <- c(column_order, other_columns)
     df_ph_updated <- df_ph_updated[, new_order]
-    
-    for (i in 6:length(df_ph_updated)) {
-      pt1 <- df_ph_updated[, 1:5]
+
+    for (i in start_pos:length(df_ph_updated)) {
+      pt1 <- df_ph_updated[, 1:length(column_order)]
       patients = cbind(pt1, df_ph_updated[, i])
-      colnames(patients)[1:6] = c("UNIPROT", "aminoacid", "position", "gene_name", "sequence_window", "difference")
+      colnames(patients)[1:(start_pos)] = c(column_order, "difference")
       patients = patients %>%
         dplyr::mutate(logpval = 2 * pnorm(difference, lower.tail = FALSE))
       patients = patients %>%
