f75855c38288b3aa203d497c296658727c264066
veronicavenafra <veronica.venafra97@gmail.com>
2025-03-27 15:35:00 +0100

    header=F extract_sign

--- R/extract_signatures.R
+++ R/extract_signatures.R
@@ -1,12 +1,12 @@
 
 
 #' Extract signaling-driven transcriptomics signature from communities
-#' 
+#'
 #' This function analyzes transcriptomic data by performing ANOVA-Tukey on gene expression levels and then generating community-specific gene expression signatures
 #'
 #' @param base_path Character. Path to the directory containing community folders.
-#' @param transcriptomics_file Character. Path to the transcriptomic data file. 
-#' @param output_dir Character. Path to the directory containing all function's results. 
+#' @param transcriptomics_file Character. Path to the transcriptomic data file.
+#' @param output_dir Character. Path to the directory containing all function's results.
 #' @param padj_thres Numeric. Adjusted p-value threshold for filtering significant results. Default: `0.01`.
 #' @param diff_thres Numeric. Minimum difference threshold for gene expression filtering. Default: `0.7`.
 #' @param mean_exp_clus_thres Numeric. Minimum mean expression threshold for gene filtering. Default: `0`.
@@ -18,8 +18,8 @@
 #' @export
 #'
 #' @examples
-#' extract_signatures <- function(base_path = "./vignette/Communities/output_communities/",          
-#' transcriptomics_file = "./PatientProfiler_processed_input/Transcriptomics_updated.tsv", 
+#' extract_signatures <- function(base_path = "./vignette/Communities/output_communities/",
+#' transcriptomics_file = "./PatientProfiler_processed_input/Transcriptomics_updated.tsv",
 #' output_dir = "./Anova_result.tsv",
 #' padj_thres = 0.01,
 #' diff_thres = 0.5,
@@ -28,72 +28,72 @@
 
 
 
-extract_signatures <- function(base_path,          
-                               transcriptomics_file, 
+extract_signatures <- function(base_path,
+                               transcriptomics_file,
                                output_dir,
                                padj_thres = 0.01,
                                diff_thres = 0.5,
                                mean_exp_clus_thres = 0,
                                max_val = 50) {
-  
+
   message("Warning: This function may take a long time to execute.")
-  
+
   message("Creating output directories...")
   dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
-  
+
   output_file <- file.path(output_dir, "Anova_result.tsv")
-  
+
   signature_dir <- file.path(output_dir, "Signatures")
   dir.create(signature_dir, showWarnings = FALSE)
-  
+
   message("Scanning community folders...")
   community_folders <- list.dirs(base_path, recursive = FALSE)
   stratification_table <- data.frame()
-  
+
   for (community_folder in community_folders) {
     community_id <- basename(community_folder)
-    patients_file <- file.path(community_folder, "patients.txt") 
-    
+    patients_file <- file.path(community_folder, "patients.txt")
+
     if (file.exists(patients_file)) {
       message(paste("Processing", patients_file))
-      community_data <- read.table(patients_file, header = TRUE, stringsAsFactors = FALSE)
-      colnames(community_data)[1] <- "Patient_ID"  
-      community_data$community <- community_id  
+      community_data <- read.table(patients_file, header = FALSE, stringsAsFactors = FALSE)
+      colnames(community_data)[1] <- "Patient_ID"
+      community_data$community <- community_id
       stratification_table <- rbind(stratification_table, community_data)
     } else {
       warning(paste("File not found:", patients_file))
     }
   }
-  
+
   message("Loading transcriptomics data...")
   Transcriptomics_patients <- readr::read_tsv(transcriptomics_file) %>%
     pivot_longer(cols = -1, names_to = "Patient_ID", values_to = "value")
   Transcriptomics_patients <- left_join(Transcriptomics_patients, stratification_table, by = 'Patient_ID')
-  
+
   message("Performing ANOVA analysis...")
   final_results <- perform_anova(Transcriptomics_patients, stratification_table)
   write_tsv(final_results, output_file)
-  
+
   message("Extracting signatures...")
   for (cluster_i in unique(final_results$cluster)) {
     message(paste("Processing cluster:", cluster_i))
     final_results_filtered <- final_results %>%
       dplyr::arrange(`p adj`) %>%
       dplyr::filter(cluster == cluster_i &
-                      `p adj` < padj_thres & 
-                      diff > diff_thres & 
+                      `p adj` < padj_thres &
+                      diff > diff_thres &
                       mean_exp_clus > mean_exp_clus_thres)
-    
+
     if (nrow(final_results_filtered) > max_val) {
       message(paste("Limiting to", max_val, "entries for cluster", cluster_i))
       final_results_filtered <- final_results_filtered[1:max_val,]
     }
-    
+
     output_signature_file <- file.path(signature_dir, paste0('signature_', cluster_i, '.tsv'))
     write_tsv(final_results_filtered[, 5:6], output_signature_file)
     message(paste("Signature saved to", output_signature_file))
   }
-  
+
   message("Process completed.")
   return(final_results)
 }
